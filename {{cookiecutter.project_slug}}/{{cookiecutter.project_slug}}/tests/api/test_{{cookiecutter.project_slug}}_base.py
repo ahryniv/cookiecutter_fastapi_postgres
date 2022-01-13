286b29a3c03970443bb2de2ff08de09716da0378
zakko13 <varsallll@gmail.com>
2022-01-14 00:33:09 +0200

    fix test names

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/api/test_{{cookiecutter.project_slug}}_base.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/api/test_{{cookiecutter.project_slug}}_base.py
@@ -3,11 +3,11 @@ from http import HTTPStatus
 from fastapi.testclient import TestClient
 
 
-def test_health(client: TestClient):
+def test_version(client: TestClient):
     response = client.get('version')
     assert response.status_code == HTTPStatus.OK
 
 
-def test_version(client: TestClient):
+def test_health(client: TestClient):
     response = client.get('health')
     assert response.status_code == HTTPStatus.OK
